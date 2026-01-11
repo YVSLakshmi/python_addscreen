013bf9a93d4586cbb8f0b1ae257813bfa93180aa
yvslakshmi262 <yvsubbalakshmi6@gmail.com>
2026-01-11 22:36:43 +0530

    Change eemail field type from EmailField to CharField

--- add screen/employee/migrations/0001_initial.py
+++ add screen/employee/migrations/0001_initial.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('eid', models.CharField(max_length=20)),
                 ('ename', models.CharField(max_length=100)),
-                ('eemail', models.EmailField(max_length=254)),
+                ('eemail', models.CharField(max_length=100)),
                 ('econtact', models.CharField(max_length=15)),
             ],
             options={
